c5ab85b04f47e6b596edcd23eee2161867a345a1
SofiaN-t <soph.miscel@gmail.com>
2023-12-17 20:54:48 +0100

    homepage

--- 0_👋_Home.py
+++ 0_👋_Home.py
@@ -1,10 +1,10 @@
 import streamlit as st
 import base64
 
-    
+name = "Sofia Ntalla"
 # ----- Page configs (tab title, favicon) -----
 st.set_page_config(
-    page_title="Sofia Ntalla Portfolio",
+    page_title=f"{name}'s Portfolio",
     page_icon="📊",
 )
 
@@ -15,16 +15,16 @@ with st.sidebar:
     st.header("Introduction to Programming Languages for Data")
     st.write("###")
     st.write("***Final Project - Dec 2023***")
-    st.write("**Author:** Sofia Ntalla")
+    st.write(f"**Author:** {name}")
     st.write("**Instructor:** [Enric Domingo](https://github.com/enricd)")
 
 
 # ----- Top title -----
-st.write(f"""<div style="text-align: center;"><h1 style="text-align: center;">👋 Hi! My name is Sofia</h1></div>""", unsafe_allow_html=True)  # TODO: Add your name
+st.write(f"""<div style="text-align: center;"><h1 style="text-align: center;">👋 Hi! My name is {name}</h1></div>""", unsafe_allow_html=True)
 
 
 # ----- Profile image file -----
-profile_image_file_path = "profile_image.png"       # TODO: Upload your profile image to the same folder as this script and update this if it has a different name
+profile_image_file_path = "profile.jpeg"       
 
 with open(profile_image_file_path, "rb") as img_file:
     img = "data:image/png;base64," + base64.b64encode(img_file.read()).decode()
@@ -33,15 +33,17 @@ with open(profile_image_file_path, "rb") as img_file:
 # ----- Your Profile Image -----
 st.write(f"""
 <div style="display: flex; justify-content: center;">
-    <img src="{img}" alt="Sofia Ntalla" width="300" height="300" style="border-radius: 50%; object-fit: cover; margin-top: 40px; margin-bottom: 40px;">
+    <img src="{img}" alt=f"{name}" width="300" height="300" style="border-radius: 50%; object-fit: cover; margin-top: 40px; margin-bottom: 40px;">
 </div>
 """, unsafe_allow_html=True)
 
 
 # ----- Personal title or short description -----
-current_role = "Current Role and/or Studies or Description"   # TODO: Change this
+current_role = "Student at EAE Business School"
+course = "Big Data & Analytics"
 
 st.write(f"""<div style="text-align: center;"><h4><i>{current_role}</i></h4></div>""", unsafe_allow_html=True)
+st.write(f"""<div style="text-align: center;"><h6><i>{course}</i></h6></div>""", unsafe_allow_html=True)
 
 st.write("##")    # Adding some space
 
@@ -49,21 +51,19 @@ st.write("##")    # Adding some space
 # ----- About me section -----
 st.subheader("About Me")
 
-# TODO: Modify and adapt the following lines to your info, you can add or remove some details if you want
+# 
 st.write("""
-- 🧑‍💻 I am a <Current Role and/or Studies> 
+- 🧑‍💻 Currently a student
 
-- 🛩️ prev: <Previous Experience, Background or Studies>
+- 🛩️ Previously: Technologist at Tata Steel
 
-- ❤️ <Your Passion and/or Interests>
+- 🏂 Writing at Greek feminist blog, swinning
 
-- 🤖 <Your Personal Projects>
-
-- 🏂 <Your Hobbies>
-
-- 📫 How to reach me: <Your Email>
+- 📫 How to reach me: sophia.ntalla@gmail.com
 
 - 🏠 Barcelona
+        
+- 💼 LinkedIn: www.linkedin.com/in/sofia-ntalla       
 """)
 
 # Feel free to add other points like your Linkedin, Github, Social Media, etc.
